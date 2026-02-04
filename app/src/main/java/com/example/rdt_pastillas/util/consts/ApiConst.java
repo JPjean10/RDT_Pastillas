1a9bc274cc4ef53b1b05a73d31957e1dcb080f9e
jean <jeanpauldc90@gmail.com>
2026-02-04 08:03:08 -0500

    listo para lanzar a producion

--- app/src/main/java/com/example/rdt_pastillas/util/consts/ApiConst.java
+++ app/src/main/java/com/example/rdt_pastillas/util/consts/ApiConst.java
@@ -3,9 +3,9 @@ package com.example.rdt_pastillas.util.consts;
 public class ApiConst {
 
     // cabeseras
-    public static final String USUARIO = "/usuario";
-    public static final String GLUCOSA = "/glucosa";
-    public static final String PRESION = "/presion";
+    public static final String USUARIO = "/mi-salud/usuario";
+    public static final String GLUCOSA = "/mi-salud/glucosa";
+    public static final String PRESION = "/mi-salud/presion";
     // usuario______________________________________________________________________________________
     public static final String LOGIN = "/Login";
     //______________________________________________________________________________________________
